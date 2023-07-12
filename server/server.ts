298e9e8b5e8226e483f5d960195511c7909843f0
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-12 23:05:12 +1000

    sess fucking working aaa, update your docs please

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,14 @@
+//tried to use ts, but cant import and cant be bothered
+
 const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +17,23 @@ const server = new ApolloServer({
   resolvers,
 });
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/craproject',
+    ttl: 14 * 24 * 60 * 60,
+  }),
+};
+
+app.use(session(sess));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -19,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
