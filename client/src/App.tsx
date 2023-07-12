fdb2f55bec19db3d22c287817eb57397eab03181
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-12 20:30:30 +1000

    client config done fml

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
+
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -12,9 +13,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div>
 
-        </div>
       </Router>
     </ApolloProvider>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,36 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: [],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: [
-      { 
-        'mytheme': {
-          "primary": "#fcbdbe",     
-          "secondary": "#072784",         
-          "accent": "#0c2668",    
-          "neutral": "#3c2442",    
-          "base-100": "#2f2c3a",    
-          "info": "#4190c8",    
-          "success": "#3cd3af",    
-          "warning": "#ecc251",    
-          "error": "#fa3314",
-        },  
-      },
-    ],
-  },
-};
+  plugins: [],
+}
 
