01e954712d6d155b8cd2f99c12f8e2502bc4f1da
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-15 15:52:06 +1000

    navbar added

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -14,11 +14,30 @@ export default function Navbar () {
         }
     };
 
+    const classNames = ['btn-ghost', 'btn-primary', 'btn-info', 'btn-success', 'btn-warning',];
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleClick = () => {
+
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % classNames.length);
+      };
+
     return (
-        <div>
-            <form className='text-black' onSubmit={handleSubmit}>
-                <button type="submit" className='text-white'>Logout</button>
-            </form>
-        </div>
+        <nav className='navbar justify-center '>
+            <div className="flex-1">
+            <button className={`btn normal-case text-xl ${classNames[currentIndex]}`} onClick={handleClick}>C.R.A.P</button>
+            </div>
+            
+            <div className="dropdown dropdown-end">
+                <button className="btn btn-square btn-ghost">
+                    <svg tabIndex={0} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </button>
+
+                <form tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-2">
+                    <button className=' mb-1 border rounded-xl ' >Settings</button>
+                    <button className=' mb-1 border rounded-xl ' onClick={handleSubmit}>Logout</button> 
+                </form>
+            </div>
+        </nav>
     )
 }
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -6,8 +6,6 @@ export default function Home () {
   return (
     <div>
       <h1>Welcome to the home page!</h1>
-      <Link to='/signup'>Signup</Link>
-      <Link to='/login'>Login</Link>
     </div>
   );
 }
