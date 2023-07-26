8e8c5cbc9a07fcbfaece9e58d7acaaaae597d346
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-26 17:33:46 +1000

    api keys in .env

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
-const stripe = require('stripe')('sk_test_51NXgzkGNThYXIbcn0F6aZpnthhJGrpCwWJ9zBoEx4tciJdgeqHbeAShEflBV586w2T8DZb9XxxTGSfBtXOWdt1WB00FBzaq8J9');
-
-const PORT = process.env.PORT || 3001;
+const SK = provess.env.STRIPE_SK_TEST;
+const stripe = require('stripe')(SK);
+ 
+const YOUR_DOMAIN = 'http://localhost:3000';
 
 router.post('/checkout', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: 'price_1NY1xIGNThYXIbcnxPscPPKY',
+          price: 'price_1NY29eGNThYXIbcnsHjHceUV',
           quantity: 1,
         },
       ],
       mode: 'payment',
-      success_url: `http://localhost:3000?success=true`,
-      cancel_url: 'http://localhost:3000?canceled=true',
+      success_url: `${YOUR_DOMAIN}?success=true`,
+      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
   
     res.redirect(303, session.url);
