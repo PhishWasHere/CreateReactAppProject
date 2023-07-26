a2ac27172aef811b8baf5f1a565ea02700b71cc4
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-26 17:20:23 +1000

    stripe api intergration v2

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,5 +1,23 @@
 const router = require('express').Router();
+const stripe = require('stripe')('sk_test_51NXgzkGNThYXIbcn0F6aZpnthhJGrpCwWJ9zBoEx4tciJdgeqHbeAShEflBV586w2T8DZb9XxxTGSfBtXOWdt1WB00FBzaq8J9');
 
+const PORT = process.env.PORT || 3001;
 
+router.post('/checkout', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1NY1xIGNThYXIbcnxPscPPKY',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `http://localhost:3000?success=true`,
+      cancel_url: 'http://localhost:3000?canceled=true',
+    });
+  
+    res.redirect(303, session.url);
+});
 
 module.exports = router;
