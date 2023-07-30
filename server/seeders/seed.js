c65476437670b732a792cfae2770ee7d5e0f4643
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-07-30 20:32:03 +1000

    seed data done

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,25 +1,28 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Project } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const projectSeeds = require('./projectSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Thought.deleteMany({});
+    await Project.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
+    for (let i = 0; i < projectSeeds.length; i++) {
+      const { user, ...projectData } = projectSeeds[i];
+      const userName = await User.findOne({ username: user });
+
+      if (!user) {
+        console.error(`User with username "${user}" not found. Skipping project creation.`);
+        continue;
+      }
+      
+      const project = new Project({ user: userName._id, ...projectData });
+      await project.save();
+      console.log(project);
+
     }
   } catch (err) {
     console.error(err);
